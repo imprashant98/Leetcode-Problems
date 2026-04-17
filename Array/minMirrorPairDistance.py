f788d224b5bea3750e53d6d3303b50a110db2af6
Prashant <imprashant1802080@gmail.com>
2026-04-17 09:37:56 +0545

    minMirrorPairDistance

--- Array/minMirrorPairDistance.py
+++ Array/minMirrorPairDistance.py
@@ -1,14 +1,32 @@
 from typing import List
+from math import inf
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        buy_price = prices[0]
-        profit = 0
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        def reverse(x: int) -> int:
+            y = 0
+            while x:
+                v = x % 10
+                y = y * 10 + v
+                x //= 10
+            return y
 
-        for p in prices[1:]:
-            if buy_price > p:
-                buy_price = p
-            
-            profit = max(profit, p-buy_price)
-        
-        return profit
+        pos = {}
+        ans = inf
+        for i, x in enumerate(nums):
+            print(pos,"Position")
+            print(x,"X")
+            print (reverse(x),"Reverse X")
+            print(i,"I")
 
+            if x in pos:
+                ans = min(ans, i - pos[x])
+            pos[reverse(x)] = i
+            print(ans,"Answer")
+        return -1 if ans == inf else ans
+    
+
+    #Example usage
+solution_instance = Solution()
+nums = [123, 321, 456, 654, 789]
+result = solution_instance.minMirrorPairDistance(nums)
+print(result)
